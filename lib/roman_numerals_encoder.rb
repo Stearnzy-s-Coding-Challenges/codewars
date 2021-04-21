90b65bf01e128eb9392d9908d67af6ee4ab276c7
Zach Stearns <zach.stearns92@gmail.com>
2021-04-21 12:55:43 -0600

    Create numerical dictionary and start solution method to pass first test

--- lib/roman_numerals_encoder.rb
+++ lib/roman_numerals_encoder.rb
@@ -1,3 +1,24 @@
 class RomanNumeralsEncoder
-  
+  NUMERALS = {
+    1000 => 'M',
+    500 => 'D',
+    100 => 'C',
+    50 => 'L',
+    10 => 'X',
+    5 => 'V',
+    1 => 'I'
+  }
+
+  def solution(number)
+    result = ''
+
+    NUMERALS.each do |num_key, letter|
+      if number / num_key >= 1
+        result.concat(letter)
+        number -= num_key
+      end
+    end
+
+    result
+  end
 end
