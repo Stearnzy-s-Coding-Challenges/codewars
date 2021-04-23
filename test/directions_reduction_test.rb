612820c6cca6d8c9ae3f66fac45cbf49d9b55e6b
Zach Stearns <zach.stearns92@gmail.com>
2021-04-23 16:47:08 -0600

    Add simple and complex testing

--- test/directions_reduction_test.rb
+++ test/directions_reduction_test.rb
@@ -9,4 +9,45 @@ class DirectionsReductionTest < Minitest::Test
     assert_instance_of DirectionsReduction, reducer
   end
 
+  def test_it_can_return_one_direction
+    reducer = DirectionsReduction.new
+    directions = ['EAST']
+
+    assert_equal ['EAST'], reducer.dirReduc(directions)
+  end
+
+  def test_it_cancels_out_simple_east_west
+    reducer = DirectionsReduction.new
+    directions = %w[EAST WEST]
+
+    assert_equal [], reducer.dirReduc(directions)
+  end
+
+  def test_it_cancels_out_simple_north_south
+    reducer = DirectionsReduction.new
+    directions = %w[NORTH SOUTH]
+
+    assert_equal [], reducer.dirReduc(directions)
+  end
+
+  def test_it_cancels_out_more_complex_north_south
+    reducer = DirectionsReduction.new
+    directions = %w[NORTH SOUTH SOUTH SOUTH NORTH NORTH SOUTH NORTH]
+
+    assert_equal [], reducer.dirReduc(directions)
+  end
+
+  def test_it_cancels_out_simple_combination
+    reducer = DirectionsReduction.new
+    directions = %w[NORTH EAST SOUTH WEST]
+
+    assert_equal %w[NORTH EAST SOUTH WEST], reducer.dirReduc(directions)
+  end
+
+  def test_it_cancels_out_complex_combination
+    reducer = DirectionsReduction.new
+    directions = %w[NORTH SOUTH SOUTH EAST WEST NORTH WEST]
+
+    assert_equal ['WEST'], reducer.dirReduc(directions)
+  end
 end
