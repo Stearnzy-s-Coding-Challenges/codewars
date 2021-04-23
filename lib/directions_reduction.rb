1c8fb3e85835667d017a7a65c4c9e40c1372d19a
Zach Stearns <zach.stearns92@gmail.com>
2021-04-23 16:47:29 -0600

    Complete method, CW passing

--- lib/directions_reduction.rb
+++ lib/directions_reduction.rb
@@ -1,4 +1,20 @@
 class DirectionsReduction
+  OPPOSITES = {
+    'NORTH' => 'SOUTH',
+    'WEST' => 'EAST',
+    'SOUTH' => 'NORTH',
+    'EAST' => 'WEST'
+  }
+
   def dirReduc(directions)
+    acc = []
+    directions.each do |dir|
+      if OPPOSITES[dir] == acc.last
+        acc.pop
+      else
+        acc << dir
+      end
+    end
+    acc
   end
 end
