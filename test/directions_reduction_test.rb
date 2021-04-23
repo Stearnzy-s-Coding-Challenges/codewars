58b591dd5b0a7aec664c7e6aedf6e24e0e03c9a4
Zach Stearns <zach.stearns92@gmail.com>
2021-04-23 11:47:46 -0600

    Create instance test

---
+++ test/directions_reduction_test.rb
@@ -0,0 +1,11 @@
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/directions_reduction'
+
+class DirectionsReductionTest < Minitest::Test
+  def test_it_exists
+    reducer = DirectionsReduction.new
+    assert_instance_of DirectionsReduction, reducer
+  end
+end
