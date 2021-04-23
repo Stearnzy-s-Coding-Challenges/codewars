eff2495795e42efdde22137f71c26b5e1c3ebd6c
Zach Stearns <zach.stearns92@gmail.com>
2021-04-23 11:57:32 -0600

    WIP, starting to write out method

--- lib/directions_reduction.rb
+++ lib/directions_reduction.rb
@@ -1,3 +1,16 @@
 class DirectionsReduction
-  
+  def dirReduc(directions)
+    cardinal_directions = { 
+      'NORTH' => 0,
+      'WEST' => 0,
+      'SOUTH' => 0,
+      'EAST' => 0,
+     }
+    
+     directions.each do |dir|
+      cardinal_directions[dir] += 1
+     end
+
+     
+  end
 end
